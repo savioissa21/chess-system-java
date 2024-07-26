771c97a30a3fd68dc18981b60071475f0c8143d3
Savio Issa <savioissadesousa@gmail.com>
2024-07-25 22:41:20 -0300

    Utilizando exception personalizada(BoardException)

--- src/main/java/Bordergame/Board.java
+++ src/main/java/Bordergame/Board.java
@@ -6,6 +6,9 @@ public class Board {
     private Piece[][] pieces;
 
     public Board(int rows, int columns) {
+        if(rows < 1 || columns < 1) {
+            throw new BoardException("Erro : Ao criar o tabuleiro é nescessario que haja pelo menos uma linha e uma coluna ");
+        }
         this.rows = rows;
         this.columns = columns;
         pieces = new Piece[rows][columns];
@@ -15,26 +18,43 @@ public class Board {
         return rows;
     }
 
-    public void setRows(int rows) {
-        this.rows = rows;
-    }
-
     public int getColumns() {
         return columns;
     }
 
-    public void setColumns(int columns) {
-        this.columns = columns;
-    }
     public Piece piece(int row, int column) {
+        if(!positionExists(row, column)) {
+            throw new BoardException("Posição não existe no tabuleiro");
+        }
         return pieces[row][column];
     }
+
     public Piece piece(Position position) {
+        if(!positionExists(position)) {
+            throw new BoardException("Posição não existe no tabuleiro");
+        }
         return pieces[position.getRow()][position.getColumn()];
     }
-public void placePiece(Piece piece, Position position) {
+
+    public void placePiece(Piece piece, Position position) {
+        if(thereIsPiece(position)) {
+            throw new BoardException("Já possui uma peça nessa posição " + position);
+        }
         pieces[position.getRow()][position.getColumn()] = piece;
         piece.position = position;
+    }
 
-}
+    private boolean positionExists(int row, int column) {
+return row >= 0 && row < rows && column >= 0 && column < columns;
+    }
+
+    public boolean positionExists(Position position) {
+        return positionExists(position.getRow(), position.getColumn());
+    }
+    public boolean thereIsPiece(Position position) {
+        if(!positionExists(position)) {
+            throw new BoardException("Posição não existe no tabuleiro");
+        }
+        return piece(position) != null;
+    }
 }
