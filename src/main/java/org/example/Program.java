4e4dc8b999bd000aaa0a88c2c06994973e84a22e
Savio Issa <savioissadesousa@gmail.com>
2024-07-25 21:34:29 -0300

    Iniciando implementação do tabuleiro e peças

--- src/main/java/org/example/Program.java
+++ src/main/java/org/example/Program.java
@@ -1,10 +1,11 @@
 package org.example;
 
+import Bordergame.Board;
 import Bordergame.Position;
 
 public class Program {
     public static void main(String[] args) {
-        Position pos = new Position(3, 5);
-        System.out.println(pos);
+        Board board = new Board(8,8);
+        
     }
 }
