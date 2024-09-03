7581e3be0863ca195210b2c5a73b33cacbcf34a5
Savio Issa <savioissadesousa@gmail.com>
2024-09-03 08:52:16 -0300

    Knight

--- src/main/java/chess/ChessMatch.java
+++ src/main/java/chess/ChessMatch.java
@@ -9,6 +9,7 @@ import Bordergame.Piece;
 import Bordergame.Position;
 import chess.pieces.Bishop;
 import chess.pieces.King;
+import chess.pieces.Knight;
 import chess.pieces.Pawn;
 import chess.pieces.Rook;
 
@@ -199,13 +200,13 @@ public class ChessMatch {
         System.out.println("Placing " + piece + " at " + column + row); // Mensagem de debug
         board.placePiece(piece, new ChessPosition(column, row).toPosition());
         piecesONTheBoard.add(piece);
-    }
-
-    private void initialSetup() {
+    }private void initialSetup() {
         placeNewPiece('a', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('b', 1, new Knight(board, Color.WHITE));
         placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
         placeNewPiece('e', 1, new King(board, Color.WHITE));
         placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
+        placeNewPiece('g', 1, new Knight(board, Color.WHITE));
         placeNewPiece('h', 1, new Rook(board, Color.WHITE));
         placeNewPiece('a', 2, new Pawn(board, Color.WHITE));
         placeNewPiece('b', 2, new Pawn(board, Color.WHITE));
@@ -217,9 +218,11 @@ public class ChessMatch {
         placeNewPiece('h', 2, new Pawn(board, Color.WHITE));
 
         placeNewPiece('a', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('b', 8, new Knight(board, Color.BLACK));
         placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
         placeNewPiece('e', 8, new King(board, Color.BLACK));
         placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
+        placeNewPiece('g', 8, new Knight(board, Color.BLACK));
         placeNewPiece('h', 8, new Rook(board, Color.BLACK));
         placeNewPiece('a', 7, new Pawn(board, Color.BLACK));
         placeNewPiece('b', 7, new Pawn(board, Color.BLACK));
--- src/main/java/chess/pieces/Bishop.java
+++ src/main/java/chess/pieces/Bishop.java
@@ -1,5 +1,62 @@
 package chess.pieces;
 
-public class Bishop {
+import Bordergame.Board;
+import Bordergame.Position;
+import chess.ChessPiece;
+import chess.Color;
 
+     public class Bishop extends ChessPiece {
+       public Bishop (Board board, Color color) {
+        super(board, color);
+    }
+
+    @Override
+    public String toString() {
+        return "B";
+    }
+
+    @Override
+    public boolean[][] possibleMove() {
+        boolean[][] m = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+        Position p = new Position(0,0);
+        //NW
+        p.setValue(position.getRow() -1 , position.getColumn() -1);
+        while(getBoard().positionExists(p) && !getBoard().thereIsPiece(p)) {
+            m[p.getRow()][p.getColumn()] = true;
+            p.setValue(p.getRow() -1, p.getColumn() -1);
+        }
+        if (getBoard().positionExists(p) && isThereOpponent(p)){
+            m[p.getRow()][p.getColumn()] = true;
+        }
+        //NE
+        p.setValue(position.getRow() -1 , position.getColumn()+1);
+        while(getBoard().positionExists(p) && !getBoard().thereIsPiece(p)) {
+            m[p.getRow()][p.getColumn()] = true;
+            p.setValue(p.getRow() - 1, p.getColumn() +1);;
+        }
+        if (getBoard().positionExists(p) && isThereOpponent(p)){
+            m[p.getRow()][p.getColumn()] = true;
+        }
+        //SE
+        p.setValue(position.getRow() +1, position.getColumn()+1);
+        while(getBoard().positionExists(p) && !getBoard().thereIsPiece(p)) {
+            m[p.getRow()][p.getColumn()] = true;
+            p.setValue(p.getRow() + 1, p.getColumn() +1);
+        }
+        if (getBoard().positionExists(p) && isThereOpponent(p)){
+            m[p.getRow()][p.getColumn()] = true;
+        }
+        //SW
+        p.setValue(position.getRow() +1 , position.getColumn() -1);
+        while(getBoard().positionExists(p) && !getBoard().thereIsPiece(p)) {
+            m[p.getRow()][p.getColumn()] = true;
+            p.setValue(p.getRow() + 1, p.getColumn() -1);;
+        }
+        if (getBoard().positionExists(p) && isThereOpponent(p)){
+            m[p.getRow()][p.getColumn()] = true;
+        }
+
+        return m;
+    }
 }
